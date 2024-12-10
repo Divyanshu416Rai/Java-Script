e43dcc1d23a34b05921bfe65d91df81004328d53
Divyanshu416Rai <divyanshukumarrai832@gmail.com>
2024-12-10 13:05:13 +0530

    y first javascript file

---
+++ script.js
@@ -0,0 +1,8 @@
+// function sayHello(){alert("HelloWorld")}
+// document.write("This is my first java script page")
+// var pi=3.14;
+// var person="are you 600 ";
+// var answer='Yes I am!';
+// document.write(pi+"<br>");
+// document.write(person+"<br>");
+// document.write(answer+"<br>");
