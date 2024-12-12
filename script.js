cd82622fa991f11368b2630723f55e0b525789bf
Divyanshu416Rai <divyanshukumarrai832@gmail.com>
2024-12-12 15:34:32 +0530

    this is added on 12/12/2024

--- script.js
+++ script.js
@@ -6,3 +6,158 @@
 // document.write(pi+"<br>");
 // document.write(person+"<br>");
 // document.write(answer+"<br>");
+
+// var person={
+//     firstname:"John",
+//     lastname:"Joe",
+//     id :5566,
+//     getinfo : function(){
+//         return this.firstname+""+this.lastname;
+//     }
+//     };
+//     document.getElementByld("demo").innerHTML=person.getinfo();
+// var person ={
+//     name:'Ram',
+//     age:30,
+//     isMarried:true,
+//     address:{
+//         street : 'Vijay Nagar',
+//         flatNo:241
+//     }
+// }
+
+
+// function sayHello() {
+//     alert("Hello World");
+// }
+
+// document.write("This is my first JavaScript page<br>");
+// var pi = 3.14;
+// var personStatement = "Are you 600?";
+// var answer = "Yes, I am!";
+// document.write(pi + "<br>");
+// document.write(personStatement + "<br>");
+// document.write(answer + "<br>");
+
+// var person = {
+//     firstname: "John",
+//     lastname: "Joe",
+//     id: 5566,
+//     getInfo: function() {
+//         return this.firstname + " " + this.lastname; 
+//     }
+// };
+
+
+// document.getElementById("demo").innerHTML = person.getInfo();
+
+
+// var anotherPerson = {
+//     name: "Ram",
+//     age: 30,
+//     isMarried: true,
+//     address: {
+//         street: "Vijay Nagar",
+//         flatNo: 241
+//     }
+// };
+// console.log("Name: " + anotherPerson.name);
+// console.log("Street: " + anotherPerson.address.street);
+// var person={
+//     name:'RAI',
+//     age:20,
+//     ismarried:true,
+//     child:['cheeku'],
+//     getInfo:function(){
+//     },
+//     address:{
+//         street:'vinay nagar',
+//         flatno:201
+//     }
+// };
+// console.log(person['name'])
+// document.write(person['name'])
+// function myFunction(){
+//     var carname="Volvo";
+//     document.getElementById("demo1").innerHTML=typeof carname=+""+carname;
+// }
+// myfunction();
+// document.getElementByld("demo2").innerHTML=typeof carName;
+
+// function sayHello(){
+//     alert("Hello World")}
+// document.write('<h1>This is my first JavaScript Page</h1>');
+// var pi=3.14;
+// var person="are you GODF";
+// var answer='Yes I am!';
+// document.write(pi + "<br>")
+// document.write(person + "<br>")
+// document.write(answer + "<br>")
+
+// function myFunction(){
+//     var carname="Welcome";
+//     document.getElementById("demo").innerHTML=carname;
+// }
+// var num=10;
+// console.log(num);
+// console.log(typeof(num));
+// num=false;
+// console.log(num);
+// console.log(typeof(num));
+// num="Krishan Kant Jha"
+// console.log(num);
+// console.log(typeof(num));
+// num=null
+// console.log(num);
+// console.log(typeof(num));
+// var num=undefined;
+// console.log(num);
+// console.log(typeof(num));
+
+// var person = {
+//     firstName:"John",
+//     lastName:"Doe",
+//     id :5566,
+//     getinfo:function(){
+//         return this.firstName+" "+this.lastName;
+//     }
+// };
+// document.getElementById("demo").innerHTML=person.getinfo();
+
+// var person= {
+//     name:'Ramesh',
+//     age:30,
+//     isMarried:true,
+//     address:{
+//         street:'vinay nagar,
+//         flatNO:201
+//     }
+// };
+// console.log(person);
+
+// var person2=new Object()
+// person2.name='rai';
+// person2.age=32;
+// person2.isMarried=true;
+// person2.address={};
+// person2.address.street="nagar road";
+// person2['address']['flatNO']=33;
+// console.log(person2);
+// var person={
+//     name:'Jitendra Chauhan',
+//         age:30,
+//         isMarried:true,
+//         child:['cheeku'],
+//         getInfo:function(){
+//         },
+//         address : {
+//             street:'vinay nagar',
+//             flatNO:201
+//         }
+//     };
+//     console.log(person.name);
+//     document.write(person.name);
+//     document.write("<br>")
+
+
+
