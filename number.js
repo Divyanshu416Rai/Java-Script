ecfd0cba7598ee88e5bfbde9d2af5a412598584b
Divyanshu416Rai <divyanshukumarrai832@gmail.com>
2025-01-14 13:01:48 +0530

    Automatic type conversion,Regular Expression

---
+++ number.js
@@ -0,0 +1,102 @@
+// const d = new Date("2015-03-25");
+// console.log(d);
+
+// const d1 = new Date("2015-03");
+// console.log(d1);
+
+// const d2 = new Date("2015-03-25");
+// console.log(d2);
+
+
+// const d3 = new Date("2015");
+// console.log(d3);
+
+
+// const d4 = new Date("2015-03-25T12:00:00Z");
+// console.log(d4);
+
+
+// const d5=new Date("2021-03-25");
+// d5.getMonth()
+
+// const months=["January","February","March","April","May","June","July","August","September","October","November","December",];
+
+// const d6=new Date("2021-03-25");
+// d6.getHours()
+
+// const d7=["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+// d7.getTime
+
+// const d8=new Date;
+// d8.setMinutes(30)
+
+// let text="";
+// const today=new Date();
+// const someday= new Date();
+// someday.setFullYear(2100,0,14);
+
+// if (someday> today){
+//     text="today is before jaunury 14,2100";
+// }
+// else{
+//     text="today is after january 14,2100"
+// }
+
+
+// Math.round(4.6);
+// Math.round(4.5);
+// Math.round(4.4);
+
+
+// Math.ceil(-4.2);
+// Math.ceil(4.2);
+
+
+// console.log(Math.floor(2.4));
+// console.log(Math.floor(-2.4));
+
+
+
+
+// console.log(Math.max(0,12,32));
+// console.log(Math.min(2,3,5));
+
+// console.log(Math.floor(Math.random()*100));
+
+
+// function getRndIntegers(min,max){
+//     return Math.floor(Math.random()*(max-min+1))+min;
+// }
+
+// console.log(Number("3,14"));
+
+// console.log(Number(""));
+
+
+// console.log(Number("99 88"));
+
+
+// console.log(Number("john"));
+// console.log(Number(" "));
+
+// let d11= new Date();
+// console.log(Number(d11));
+
+// String(Date())
+// console.log(Date().toString());
+
+// //Automatic type Conversion
+
+// console.log(5+null);
+// console.log(5+"2");
+// console.log("5"-2);
+// console.log("4"/"2");
+// console.log("5"*"4");
+
+//Automatic string conversion
+// javastring automatically  calls the to string method when there is 
+document.getElementById("myVar")
+console.log(myVar=[1,2,3,4])
+
+
+
---
+++ regular.js
@@ -0,0 +1,15 @@
+const pattern = /e/; // Regular expression to match the letter 'e'
+let x = pattern.test("the best things in life are best"); // Test if 'e' is in the string
+console.log(x); // Output the result
+
+cars=[]
+for(let i=0;cars.length;i++){
+ text.t=cars[i]+"<br>";
+}
+
+const person= {fname:"", lname:"",age:""}
+let text="";
+for(let x in person)
+{
+    textt=person[x];
+}
